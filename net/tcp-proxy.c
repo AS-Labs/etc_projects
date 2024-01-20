9f0708036ff15c5cf1cd8ab68c6b1b2322400f5a
as-labs <a.alshaiji@protonmail.com>
2024-01-21 01:00:13 +0300

    fix(tcp-proxy.c): uncomment on_client_read function to handle client read events feat(tcp-proxy.c): add on_connection function to handle new client connections feat(tcp-proxy.c): add main function to initialize server and bind address

--- net/tcp-proxy.c
+++ net/tcp-proxy.c
@@ -35,4 +35,52 @@ void on_remote_read(uv_stream_t* remote, ssize_t nread, const uv_buf_t* buf) {
 	}
 }
 
-//void on_client_read()
+void on_client_read(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf) {
+	if (nread > 0) {
+		proxy_t* proxy = (proxy_t*)client->data;
+		uv_write(&proxy->write_req, (uv_stream_t*)&proxy->remote, buf, 1, NULL);
+	} else {
+		if (nread < 0) {
+			proxy_t* proxy = (proxy_t*)client->data;
+			uv_close((uv_handle_t*)client, on_close);
+			uv_close((uv_handle_t*)&proxy->remote, on_close);
+		}
+		free(buf->base);
+	}
+}
+
+
+void on_connection(uv_stream_t* server, int status) {
+	if (status == -1){
+		fprintf(stderr, "Connection error\n");
+		return;
+	}
+
+	proxy_t* proxy = (proxy_t*)malloc(sizeof(proxy_t));
+
+	uv_tcp_init(server->loop, &proxy->client);
+	uv_tcp_init(server->loop, &proxy->remote);
+
+	proxy->client.data = proxy;
+	proxy->remote.data = proxy;
+
+	uv_accept(server, (uv_stream_t*)&proxy->client);
+
+	struct sockaddr_in remote_addr;
+	uv_ip4_addr(REMOTE_SERVER_IP, REMOTE_SERVER_PORT, &remote_addr);
+
+
+	uv_read_start((uv_stream_t*)&proxy->client, (uv_alloc_cb)uv_buf_alloc, on_client_read);
+	uv_read_start((uv_stream_t*)&proxy->remote, (uv_alloc_cb)uv_buf_alloc, on_remote_read);
+
+}
+
+int main() {
+	uv_loop_t* loop = uv_default_loop();
+
+	uv_tco_t server;
+	uv_tcp_init(loop, &server);
+
+	struct sockaddr_in bind_addr;
+	uv_ip4_addr("0.0.0.0" CLIENT_PORT, &bind_addr);
+}

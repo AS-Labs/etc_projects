3b4cff91201a290f325a23b8c981a9b61e0d798d
as-labs <as-labs@as-lap>
2024-01-21 00:19:28 +0300

    feat(sched-test.c): add initial implementation of s_Task struct for testing purposes

---
+++ linux/sched-test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// As usual we need a struct, I think? that how linux does it anyway.
+
+
+struct s_Task {
+	int pid;
+	int prio;
+	int run_time;
+	int holding_time;
+};
+

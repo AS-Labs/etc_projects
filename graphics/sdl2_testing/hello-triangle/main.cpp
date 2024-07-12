fb3f0445dadc4fce36af290d43e400ce3ec57d15
as-labs <a.alshaiji@protonmail.com>
2024-07-12 12:37:47 +0300

    Added new projects

--- graphics/sdl2_testing/hello-triangle/main.cpp
+++ graphics/sdl2_testing/hello-triangle/main.cpp
@@ -1,2 +1,6 @@
 #include <iostream>
-#include <iostream>
+#include <vector>
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wpragma-pack"
+#include <SDL/SDL.h>
+#include <SDL/SDL_vulkan.h>
